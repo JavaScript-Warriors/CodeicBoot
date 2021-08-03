8fda8c0fbb56c9db0a02e89eb2a73e0c1de3d301
BianQt <bayanqutshan@gmail.com>
2021-08-03 03:48:39 +0300

    Main Page Styling

--- js/app.js
+++ js/app.js
@@ -28,13 +28,7 @@ function generateCourses() {
   new Course('js5', 'Introduction to JavaScript.','Another great JavaScript course available for the programming greenhorns is the Introduction to JavaScript course offered by freecodecamp.org.Aimed for beginners in JS that have little to no experience working with the web scripting language, Introduction to JavaScript covers topics varying from data types and variables to JS objects and loops.One of the major attractions of this course is that a coding test follows each chapter. Hence, immediately after gulping up the new concepts, you can check how well you have grasped them with the practice exercises.',
     'js', 57,'assets/js5.png');
 
-
-
-
-
-
-
-    new Course('php1', 'JavaScript for Beginners','The JavaScript for Beginners course aims to build an in-depth understanding of the various aspects of JS that are mandatory for understanding how JavaScript applications work. With 3 hours of on-demand video, this course is one of the most compact and practical beginner-friendly JS courses. About 3k learners have benefitted from this course. You will be awarded a certificate on the successful completion to showcase all the efforts and hard work you put in for the course.',
+  new Course('php1', 'JavaScript for Beginners','The JavaScript for Beginners course aims to build an in-depth understanding of the various aspects of JS that are mandatory for understanding how JavaScript applications work. With 3 hours of on-demand video, this course is one of the most compact and practical beginner-friendly JS courses. About 3k learners have benefitted from this course. You will be awarded a certificate on the successful completion to showcase all the efforts and hard work you put in for the course.',
     'php', 150,'assets/php1.png');
   new Course('php2', 'Modern JavaScript From The Beginning.','The Modern JavaScript From the Beginning tutorial focuses on building projects without using any JS frameworks or libraries. It boasts an excellent 4.7-star rating averaged over 11k reviews. Other than English, the course is also available in Italian, Portuguese, and Spanish languages.',
     'php',199,'assets/php2.png');
@@ -54,29 +48,29 @@ generateCourses();
 
 console.log(Course.allCourses);
 
-let sale1 = document.getElementById('c-sale-1');
-let img = document.createElement('img');
-let h5 = document.createElement('h5');
-let p = document.createElement('p');
-let hr = document.createElement('hr');
-let a = document.createElement('a');
-let h4 = document.createElement('h4');
+// let sale1 = document.getElementById('c-sale-1');
+// let img = document.createElement('img');
+// let h5 = document.createElement('h5');
+// let p = document.createElement('p');
+// let hr = document.createElement('hr');
+// let a = document.createElement('a');
+// let h4 = document.createElement('h4');
 
-img.setAttribute('src', Course.allCourses[0].src);
-a.setAttribute('href','cart.html');
-p.setAttribute('style','height:92px; overflow: hidden; text-overflow: ellipsis;');
+// img.setAttribute('src', Course.allCourses[0].src);
+// a.setAttribute('href','cart.html');
+// p.setAttribute('style','height:92px; overflow: hidden; text-overflow: ellipsis;');
 
-sale1.appendChild(img);
-sale1.appendChild(h5);
-sale1.appendChild(p);
-sale1.appendChild(hr);
-sale1.appendChild(a);
-sale1.appendChild(h4);
+// sale1.appendChild(img);
+// sale1.appendChild(h5);
+// sale1.appendChild(p);
+// sale1.appendChild(hr);
+// sale1.appendChild(a);
+// sale1.appendChild(h4);
 
-h5.textContent = Course.allCourses[0].name;
-p.textContent = Course.allCourses[0].description;
-h4.textContent = Course.allCourses[0].price;
-a.textContent = 'Add to cart';
+// h5.textContent = Course.allCourses[0].name;
+// p.textContent = Course.allCourses[0].description;
+// h4.textContent = Course.allCourses[0].price;
+// a.textContent = 'Add to cart';
 
-let courseStor =JSON.stringify(Course.allCourses[0]) ;
-localStorage.setItem(Course.allCourses[0].key, courseStor);
+// let courseStor =JSON.stringify(Course.allCourses[0]) ;
+// localStorage.setItem(Course.allCourses[0].key, courseStor);
